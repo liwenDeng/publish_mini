d14c3f31d2566deea30b6364ffb95e62b19b8c86
dengliwen <dengliwen@ACB60A6D.ipt.aol.com>
2018-09-18 18:45:31 +0800

    add publish ui

--- pages/publish/detail.js
+++ pages/publish/detail.js
@@ -5,65 +5,96 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    pics: [],
+    linkman: "黄瓜啊",
+    location:'位置',
+    area:'本地',
+    price:0.1,
+    isTop: false,//是否置顶
+    topDetail:"",
+    agree: true,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+  deletePic: function(e) {
+    var picIndex = e.currentTarget.dataset.picindex;
+    var pics = this.data.pics
+    console.log('删除第',e)
+    pics.splice(picIndex,1)
+    this.setData({
+      pics
+    })
   },
   onAddPic: function(e) {
     console.log('添加图片')
-  }
+    var pics = this.data.pics
+    var that = this
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths
+        console.log(res);
+        pics = pics.concat(tempFilePaths);
+        that.setData({
+          pics
+        })
+      }
+    })
+  },
+  // 点击定位
+  onClickLocation: function(){
+    console.log('定位');
+  },
+  onClickPublishArea: function(e) {
+    console.log('选择发布地区')
+  },
+  onTopChange: function(e) {
+    var that = this;
+    this.setData({ isTop: e.detail });
+    if (this.data.isTop) {
+      //tost
+      var titles = [
+        "置顶一天（收费" + this.data.price + '元)',
+        "置顶一周（收费" + this.data.price + '元)',
+        "置顶一月（收费" + this.data.price + '元)',
+      ]
+      wx.showActionSheet({
+        itemList: titles,
+        success(res) {
+          console.log('成功', res)
+          that.setData({ topDetail: titles[res.tapIndex]})
+        },
+        fail(res){
+          console.log('取消',res)
+          that.setData({ isTop: false });
+        },
+      })
+    }
+  },
+  onAgreeChange: function(e) {
+    console.log('check');
+    this.setData({
+      agree: !this.data.agree
+    })
+  },
+  onInstructionsClick: function(e) {
+    console.log('须知');
+  },
+  formSubmit: function(e){
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
 })
--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,43 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+const formatPrice = price => {
+  /*
+   * 参数说明：
+   * number：要格式化的数字
+   * decimals：保留几位小数
+   * dec_point：小数点符号
+   * thousands_sep：千分位符号
+   * roundtag:舍入参数，默认 "ceil" 向上取,"floor"向下取,"round" 四舍五入
+   * */
+  function number_format(number, decimals, dec_point, thousands_sep, roundtag) {
+    number = (number + '').replace(/[^0-9+-Ee.]/g, '');
+    roundtag = roundtag || "ceil"; //"ceil","floor","round"
+    var n = !isFinite(+number) ? 0 : +number,
+      prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+      sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+      dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+      s = '',
+      toFixedFix = function(n, prec) {
+        var k = Math.pow(10, prec);
+        console.log();
+        return '' + parseFloat(Math[roundtag](parseFloat((n * k).toFixed(prec * 2))).toFixed(prec * 2)) / k;
+      };
+    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+    var re = /(-?\d+)(\d{3})/;
+    while (re.test(s[0])) {
+      s[0] = s[0].replace(re, "$1" + sep + "$2");
+    }
+    if ((s[1] || '').length < prec) {
+      s[1] = s[1] || '';
+      s[1] += new Array(prec - s[1].length + 1).join('0');
+    }
+    return s.join(dec);
+  }
+  return number_format(price, 2, ".")
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  formatPrice: formatPrice
 }
